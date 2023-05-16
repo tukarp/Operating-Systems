7e45c2574f5d4ad6cfc259a0f38a656c47470303
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-05-16 15:04:20 +0200

    Update Zadanie 2.c

--- Laboratoria/Zestaw 02/Zadanie 2.c
+++ Laboratoria/Zestaw 02/Zadanie 2.c
@@ -28,29 +28,29 @@ int main() {
 			scanf("%d", &potomek);
 			write(p[1], &potomek, sizeof(int));
 			
-			if(potomek==random) {
+			if(potomek == random) {
 				printf("Liczby sa rowne \n");
 			} else {
 				printf("Liczby sa rozne \n");
 			}
-			
+
 			exit(0);
 		  }
 	}
 	close(p[1]);
-	
+
 	int temp;
 	for(int i = 0; i < n; i++) {
 		read(p[0], &temp, sizeof(temp));
 		printf("%i \n", temp);
-		
-		if(temp==random) {
+
+		if(temp == random) {
 			printf("Liczby sa rowne \n");
 		}
 		else {
 			printf("Liczby sa rozne \n");
 		}
 	}
-	
+
 	return 0;
 }
